d469874e929167aa22c1a3403c5f981374f30cbb
Shreya Singh <shreya030806@gmail.com>
2025-09-18 00:48:29 +0530

    Add files via upload

---
+++ program to find sum of first natural no..c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void main()
+{
+    int i,n,sum=0;
+    printf("enter value of n\t");
+    scanf("%d",&n);
+          for(i=1;i<=n;i++)
+          {
+              sum=sum+i;
+          }
+          printf("sum =%d",sum);
+
+
+
+
+}
