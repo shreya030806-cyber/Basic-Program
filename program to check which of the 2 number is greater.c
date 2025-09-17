27917b3fc1a8611108c761035b4a5254417a38d8
Shreya Singh <shreya030806@gmail.com>
2025-09-18 00:44:11 +0530

    Add files via upload

---
+++ program to check which of the 2 number is greater.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+    int a,b;
+    printf("enter two integer number\n");
+    scanf("%d%d",&a,&b);
+    if(a>b)
+    {
+     printf("a is big\n");
+    }
+    else
+    {
+     printf("b is big\n");
+    }
+
+
+
+}
