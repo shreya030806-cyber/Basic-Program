366035c0bf0476839d72fdb6d1d00c6525c5aa38
Shreya Singh <shreya030806@gmail.com>
2025-09-25 11:46:20 +0530

    Add files via upload

---
+++ program to take input from user between 1 to 12 and print number of a days in that particular month if input is 3 and output is 31.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+
+int main(){
+  int month;
+
+  printf("Enter month number(1-12):");
+  scanf("%d",&month);
+
+  if(month==1||month==3||month==5||month==7||month==8||month==10||month==12){
+    printf("31 days\n");
+  }
+  else if(month==4||month==6||month==9||month==11){
+    printf("30 days\n");
+  }
+  else if(month==2){
+    printf("28 or 29 days(February)\n");
+  }
+  else{
+    printf ("invalid month number! please enter between 1 and 12.\n");
+  }
+
+  return 0;
+}
